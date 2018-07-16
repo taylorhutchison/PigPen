507280f340ee3f1b5134f5c02a9620257ba57cf3
Taylor Hutchison <JJ06995@JJ0000W302888.nash.tenn>
2018-07-16 13:53:46 -0500

    Added put and delete methods.

--- index.js
+++ index.js
@@ -19,11 +19,27 @@ module.exports = class PigPen {
         setup(this.app, this.config, this.store)
             .then(result => {
                 this.store.data = result;
+                logApis(this.config, result);
                 this.app.listen(this.config.port);
+                logListen(this.config);
             })
             .catch(err => {
                 console.log(err);
             })
+    }
+}
+
+const logApis = function (config, data) {
+    if (config.log) {
+        data.forEach(d => {
+            console.log(`Registered /api/${d.name}`);
+        });
+    }
+}
+
+const logListen = function (config) {
+    if (config.log) {
+        console.log(`Listening on port ${config.port}`);
     }
 }
 
@@ -58,7 +74,7 @@ const registerApi = function (apiName, config, app, store) {
                 configureResponse(app, apiName, store);
                 res({
                     name: apiName,
-                    filePath: path.join(appDir, config.data, jsonPath), 
+                    filePath: path.join(appDir, config.data, jsonPath),
                     data: data
                 });
             })
@@ -70,23 +86,43 @@ const registerApi = function (apiName, config, app, store) {
 
 const configureResponse = function (app, apiName, store) {
     app.get(`/api/${apiName}/:id?`, (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
         var data = store.get(apiName, req.params.id);
         if (data) {
+            res.setHeader('Content-Type', 'application/json');
             res.send(JSON.stringify(data));
         } else {
-            res.status(404).end(`${apiName}/${req.params.id} not found.`);
+            res.setHeader('Content-Type', 'text/plain');
+            res.status(404).send(`${apiName}/${req.params.id} not found.`);
         }
     });
     app.post(`/api/${apiName}`, (req, res) => {
         const record = store.post(apiName, req.body);
+        res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(record));
+    });
+    app.put(`/api/${apiName}/:id`, (req, res) => {
+        const record = store.put(apiName, req.body);
+        if (record) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(record));
+        } else {
+            res.setHeader('Content-Type', 'text/plain');
+            res.status(404).send(`${apiName}/${req.params.id} not found.`);
+        }
+    });
+    app.delete(`/api/${apiName}/:id`, (req, res) => {
+        if (store.delete(apiName, req.params.id)) {
+            res.status(200).end();
+        } else {
+            res.setHeader('Content-Type', 'text/plain');
+            res.status(404).send(`${apiName}/${req.params.id} not found.`);
+        }
     });
 }
 
 const getData = function (config, jsonPath) {
     return new Promise((res, rej) => {
-        const dataPath = path.join(__dirname, config.data, jsonPath);
+        const dataPath = path.join(appDir, config.data, jsonPath);
         const seedDataPath = path.join(appDir, config.seed, jsonPath);
         if (fs.existsSync(dataPath)) {
             res(readFileToJson(dataPath));
@@ -109,7 +145,7 @@ class ApiStore {
     constructor() {
         this.flushed = true;
         setInterval(() => {
-            if(!this.flushed) {
+            if (!this.flushed) {
                 this.flush();
             }
         }, 2000);
@@ -125,8 +161,9 @@ class ApiStore {
 
     get(api, id) {
         const records = this.data.find(d => d.name.toLowerCase() == api.toLowerCase()).data;
+        const convertedId = isNaN(parseInt(id)) ? id : parseInt(id);
         if (id) {
-            return records.find(r => r.id == id);
+            return records.filter(r => r.id == convertedId)[0];
         }
         return records;
     }
@@ -146,11 +183,33 @@ class ApiStore {
     }
 
     put(api, data) {
+        if (!data && !data.id) {
+            return null;
+        }
+        const records = this.data.find(d => d.name.toLowerCase() == api.toLowerCase()).data;
+        const record = records.filter(r => r.id === data.id)[0];
+        if (!record) {
+            return null;
+        }
+        Object.assign(record, data);
         this.flushed = false;
+        return record;
     }
 
     delete(api, id) {
-        this.flushed = false;
+        if (!id) {
+            return null;
+        }
+        const convertedId = isNaN(parseInt(id)) ? id : parseInt(id);
+        const records = this.data.find(d => d.name.toLowerCase() == api.toLowerCase());
+        const filteredRecords = records.data.filter(r => r.id !== convertedId);
+        if (records.data.length <= filteredRecords.length) {
+            return false;
+        } else {
+            records.data = filteredRecords;
+            this.flushed = false;
+            return true;
+        }
     }
 
     flush() {
@@ -164,9 +223,8 @@ class ApiStore {
 
 const configDefaults = {
     port: 3000,
-    apis: {
-
-    },
+    log: true,
+    apis: {},
     data: 'data',
     seed: 'seed'
 }
